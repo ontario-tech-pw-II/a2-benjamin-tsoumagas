9cc5202027286dadba4e0baa575c660e80381459
benjamin-tsoumagas <55511458+benjamin-tsoumagas@users.noreply.github.com>
2020-01-17 12:43:58 -0500

    Add files via upload

--- mycp.cpp
+++ mycp.cpp
@@ -1,12 +1,26 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
 int main(int argc, char const *argv[])
-{
+{	
  	ifstream fin;
  	ofstream fout;
-	
+
 	// check if there are enough arguments
-	
+	if (argc != 3){
+		cerr << "error" << endl;
+		return 1;
+	}
+	else
+	{
+		cout << "OK" << endl;
+	}
 	
 	// open the first file
+	cout << argv[1] << endl;
+	fin.open(argv[1]);
  	
 	char c;
 
@@ -18,6 +32,8 @@ int main(int argc, char const *argv[])
  	
 
 	// open the second file
+	cout << argv[2] << endl;
+	fout.open(argv[2]);
 	
  	if (fout.fail())
  	{
@@ -27,6 +43,7 @@ int main(int argc, char const *argv[])
  	
  	while(fin.get(c)) 
 	{
+		cout << c;
 		fout << c;
 	}
  	
